03875fea1b516a4868da0b091cc25514289147dc
suzhihui <wisdom.su@163.com>
2019-09-09 14:09:00 +0800

    cart

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import '@/icons' // icon test ssss
+import '@/icons' // icon test sssss
 import '@/permission' // permission control
 
 Vue.use(ElementUI, { locale })
